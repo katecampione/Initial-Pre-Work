9b532e74554b4c4672ad9a474ea9cf4dfbc3b920
Kate Campione <katecampione@gmail.com>
2019-05-21 17:40:12 -0600

    Modified array data

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,12 @@
+// I learned that unlike strings, the entries of arrays are mutable and can be changed
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+ myArray[0] = 45;
