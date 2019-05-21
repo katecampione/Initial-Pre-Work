704065d73b921474c0d70f5292e3da861c971d17
Kate Campione <katecampione@gmail.com>
2019-05-21 17:44:20 -0600

    Accessed data within a multi-dimensional array

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,7 @@
+//I learned that a multi-dimensional array is an array of arrays. When you use brackets to access your array, the first set of brackets refers to the entries in the outer-most (the first level) array, and each additional pair of brackets refers to the next level of entries inside
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
