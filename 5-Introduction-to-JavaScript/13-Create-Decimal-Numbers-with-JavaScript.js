e127f1ea0ba39d9a226173185e59cb996a00764f
Kate Campione <katecampione@gmail.com>
2019-05-21 15:16:10 -0600

    Stored a decimal number in a variable

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,7 @@
+// I learned we can store decimal numbers in variables too. Decimal numbers are sometimes referred to as floating point numbers or floats.
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
