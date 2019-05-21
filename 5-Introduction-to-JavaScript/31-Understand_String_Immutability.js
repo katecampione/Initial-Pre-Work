0591d23459b184d379b54c1c5c34598ab1b342ec
Kate Campione <katecampione@gmail.com>
2019-05-21 16:36:05 -0600

    Reassigned a variable a new string

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// I learned that string values are immutable meaning they cannot be changed. The only way to change the string would be to assign the variable a new string.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World";
