e8f4157f1552003d6d287b7ff527f27feca8ef25
Kate Campione <katecampione@gmail.com>
2019-05-21 16:37:25 -0600

    Used bracket notation to find a specific character in a string

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// I learned you can use bracket notation to fin a specific character in a string.
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
