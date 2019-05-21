4e59cd5c32b75e17e1527e5711c77cd28eebe1d0
Kate Campione <katecampione@gmail.com>
2019-05-21 15:25:22 -0600

    I found a remainder

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,8 @@
+/*The remainder operator % gives the remainder of the division of two numbers.
+In mathematics, a number can be checked to be even or odd by checking the remainder of the division of the number by 2. Ex: 17 % 2 = 1 (17 is Odd)
+48 % 2 = 0 (48 is even) */
+
+// Only change code below this line
+
+var remainder;
+remainder = 11 % 3;
