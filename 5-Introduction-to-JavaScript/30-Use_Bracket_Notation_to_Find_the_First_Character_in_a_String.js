23f30dc724b1d309d364ffcc2d2f0aa8bf70d031
Kate Campione <katecampione@gmail.com>
2019-05-21 16:33:14 -0600

    Used bracket notation to find a specific index within a string

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,15 @@
+// I learned you can use bracket notation to get a character at a specific index in a string. Zero-based indexing is used.
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName;
+firstLetterOfLastName = lastName[0];
