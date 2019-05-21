6a6d0b00eff670bd7434eb62a8f54cd090e4de0f
Kate Campione <katecampione@gmail.com>
2019-05-21 15:46:19 -0600

    Declared string variables

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+// I learned how to create a string variable. It's a string of 0 or more characters inside ""
+
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Kate";
+var myLastName = "Campione";
