8d2d3f00f83fbd30987c70ab5ca8f6961cbe3ae2
Kate Campione <katecampione@gmail.com>
2019-05-21 15:55:05 -0600

    Updated comments

--- 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -1,3 +1,3 @@
-// I learned how to put a quote inside of a string by using forward slash before the quotes
+// I learned how to put a quote inside of a string by using back slash before the quotes
 
 var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
