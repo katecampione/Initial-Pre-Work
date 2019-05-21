0a740bb23a44ae8c209b818dbbab3957c84d952a
Kate Campione <katecampione@gmail.com>
2019-05-21 14:53:53 -0600

    Assigned values to variables so they were no longer undefined

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+// I learned if you declare a variable without assigning it a value it will be undefined.
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a"
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
