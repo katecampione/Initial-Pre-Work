847a4deae07baa9b1153cf26a55d26280d07917c
Kate Campione <katecampione@gmail.com>
2019-05-21 15:31:17 -0600

    Performed a mathematical operation and assignment in one step

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,14 @@
+/* I learned the += operator will modify the contents of a variable.
+Ex: var myVar = 1;
+myVar += 5;
+console.log(myVar); // Returns 6 */
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
