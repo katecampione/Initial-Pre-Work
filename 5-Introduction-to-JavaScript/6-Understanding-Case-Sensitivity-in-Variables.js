67400ded410cf9e5f9074d26dae957ad420f77e4
Kate Campione <katecampione@gmail.com>
2019-05-21 14:57:52 -0600

    Used Camelcase when declaring my variables

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,13 @@
+//I learned that when writing variable names in JavaScript I should use camelCase. In camelCase, multi-word variable names have the first word in lowercase and the first letter of each subsequent word is capitalized.
+
+
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
