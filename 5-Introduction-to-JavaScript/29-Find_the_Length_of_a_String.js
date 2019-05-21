66e0f4785b615dbf9d1e3cbf003a5505d846803e
Kate Campione <katecampione@gmail.com>
2019-05-21 16:30:15 -0600

    Counted the length of a string value

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,17 @@
+// I learned we can count the lenght of a string value by writing .length after the string variable
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName;
+
+lastNameLength = lastName.length;
