6f4820ceef833bd83d00a30300187c06a69b4eda
Kate Campione <katecampione@gmail.com>
2019-05-21 17:29:24 -0600

    Modified an array

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+//I learned an array is a variable containing multiple values. You start an array declaration with an opening square bracket, end it with a closing square bracket, and put a comma between each entry.
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["arrays", 4];
