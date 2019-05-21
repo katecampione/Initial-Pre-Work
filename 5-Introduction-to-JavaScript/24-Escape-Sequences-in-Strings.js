37a2f15504cc503f50e45d80c91ddf07d26535e2
Kate Campione <katecampione@gmail.com>
2019-05-21 16:13:01 -0600

    Escaped sequences in a string

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,11 @@
+/*I learned that quotes are not the only characters that can be escaped in a string. Here are other examples:
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	backspace
+\f	form feed */
+
+var myStr = 'FirstLine\n\t\\SecondLine\nThirdLine' // Change this line
