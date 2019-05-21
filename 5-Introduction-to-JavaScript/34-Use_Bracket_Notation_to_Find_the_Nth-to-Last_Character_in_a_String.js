abab0e82ce01a71568cb1730a81055ff56550d35
Kate Campione <katecampione@gmail.com>
2019-05-21 16:49:05 -0600

    Retrieved the second to last letter in a string

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// I learned you can change the number to reflect the character in the string you want to retreive
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length -2];
