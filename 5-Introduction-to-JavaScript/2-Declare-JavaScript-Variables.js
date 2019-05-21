6cf9ac53d509233a77586c4281e9599ca7a66bfd
Kate Campione <katecampione@gmail.com>
2019-05-21 14:44:47 -0600

    Declared a variable using Javascript

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+//I learned JavaScript provides the following data types : undefined, null, boolean, string, symbol, number, and object. Variables are labels that point to the data
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
