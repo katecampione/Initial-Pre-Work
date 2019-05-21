8e1b4d114957773c9caf1e96de43ce47b28fc68e
Kate Campione <katecampione@gmail.com>
2019-05-21 14:48:49 -0600

    Assigned a value to a variable

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+// I learned how to assign a value to a variable. Assignment always goes from right to left.
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a;
