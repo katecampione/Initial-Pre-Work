57bb9ad3da2c59881d87a3d0a80999b27cb7f2c9
Kate Campione <katecampione@gmail.com>
2019-05-21 16:00:12 -0600

    Quoted strings with single quotes

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,4 @@
+/* I learned string values in JavaScript may be written with single or double quotes, as long as you start and end with the same type of quote. The reason why you might want to use one type of quote over the other is if you want to use both in a string. This might happen if you want to save a conversation in a string and have the conversation in quotes. A string has the same kind of quote at the beginning and end, but if you have that same quote somewhere in the middle, the string will stop early and throw an error. You can use the \ as an escape character like this:
+goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"'; */
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
