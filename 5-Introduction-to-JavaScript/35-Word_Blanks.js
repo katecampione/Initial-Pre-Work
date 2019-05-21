bbbb5fe6cebbcdf733aa1a03d043627346795a24
Kate Campione <katecampione@gmail.com>
2019-05-21 17:16:40 -0600

    Added word blanks

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,13 @@
+// I learned how to add word blanks
+
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result ="";
+  result+= "My "+myAdjective+" "+myNoun+" "+myVerb+" very "+myAdverb+".";
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
