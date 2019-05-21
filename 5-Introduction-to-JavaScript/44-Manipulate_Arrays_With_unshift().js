1081d26246ef7a56e22cda9dc1aebee474c7d97e
Kate Campione <katecampione@gmail.com>
2019-05-21 17:55:03 -0600

    Added data to beginning of array

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,15 @@
+// I learned that .shift() will add data to the end of an array, but .unshift() will add data to the beginning of an array.
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+
+myArray.unshift(["Paul", 35])
