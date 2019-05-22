905417c8a8599b83040ad6d9e663e5f919466c59
Kate Campione <katecampione@gmail.com>
2019-05-21 23:05:21 -0600

    Used greater than operator

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,17 @@
+//The greater than operator (>) compares the values of two numbers. If the number to the left is greater than the number to the right, it returns true. Otherwise, it returns false. Like the equality operator, greater than operator will convert data types of values while comparing.
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
