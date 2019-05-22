2331f34c6ee66a5c570fe8d6ea38eeab46d97f1d
Kate Campione <katecampione@gmail.com>
2019-05-21 22:45:05 -0600

    Modified Boolean function to return true

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// I learned that Boolean is a true or false statement
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
