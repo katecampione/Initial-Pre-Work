2a83642b4314a18786cb02f00d3f5d2f2a64f23f
Kate Campione <katecampione@gmail.com>
2019-05-21 22:53:45 -0600

    Added equality operator

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// I learned that the equality operator (==) compares two values and returns true if they're equivalent or false if they are not
+
+// Setup
+function testEqual(val) {
+  if (val== 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
