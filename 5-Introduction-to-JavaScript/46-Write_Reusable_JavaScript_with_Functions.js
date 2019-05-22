db4b725f32b3105bc1b0cd2787eb90562f079509
Kate Campione <katecampione@gmail.com>
2019-05-21 20:11:28 -0600

    Wrote reusable Javascript with functions

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+/*I learned we can divide up our code into reusable parts called 'functions'. You can call or invoke this function like this: functionName();
+Each time the function is called it will print out the message on the dev console. All of the code between the curly braces will be executed every time the function is called. */
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction () {
+    console.log("Hi World");
+}
+reusableFunction();
