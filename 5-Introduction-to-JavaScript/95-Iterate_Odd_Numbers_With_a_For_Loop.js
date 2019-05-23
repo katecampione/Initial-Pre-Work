8c5d3582bb8f134365898390147688d34e122800
Kate Campione <katecampione@gmail.com>
2019-05-23 15:24:15 -0600

    Pushed odd numbers to an array with for loop

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,16 @@
+//I learned that by changing the final expression we can count by even numbers and to change the initialization we can count by odd numbers
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 0; i < 5; i++) {
+  myArray.push(i+1);
+}
