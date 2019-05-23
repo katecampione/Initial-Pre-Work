223b00a7ce10ff759e8c107134226764acee3d79
Kate Campione <katecampione@gmail.com>
2019-05-23 15:39:23 -0600

    Iterated through an array with a for loop

---
+++ 5-Introduction-to-JavaScript/97-Iterate_Through_an_Array_with_a_For_Loop.js
@@ -0,0 +1,19 @@
+// I learned that you can use a for loop to iterate through the contents of an array. Remember arrays have zero based numbering.
+
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+
+for (var i = 0; i < myArr.length; i++) {
+   total += myArr[i]; // total here should equal 20
+}
