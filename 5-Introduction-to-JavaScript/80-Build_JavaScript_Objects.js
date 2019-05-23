cd37e2bc7b0283eba854a129692742e647774022
Kate Campione <katecampione@gmail.com>
2019-05-23 11:25:35 -0600

    Built javascript objects

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,33 @@
+/* I learned objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+Here's a sample cat object:
+
+var cat = {
+  "name": "Whiskers",
+  "legs": 4,
+  "tails": 1,
+  "enemies": ["Water", "Dogs"]
+};
+ */
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+   "name": "Skip",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Clifford", "Copper"]
+
+
+
+};
