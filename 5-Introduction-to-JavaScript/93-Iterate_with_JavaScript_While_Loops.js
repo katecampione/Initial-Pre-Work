f4c9812ceecf8f8b43076af6b65821c552a8714c
Kate Campione <katecampione@gmail.com>
2019-05-23 15:07:41 -0600

    Pushed numbers to an array using the while loop

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+//You can run the same code multiple times by using a loop. A "while" loop because it runs "while" a specified condition is true and stops once that condition is no longer true.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i++;
+}
