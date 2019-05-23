f5a102d7055a0e45b679034fbb59f1d573939d94
Kate Campione <katecampione@gmail.com>
2019-05-23 13:03:03 -0600

    Manipulated complex objects

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+//I learned that a jacascript object can handle flexible data and allow for abitrary combinations of strings, numbers, booleans, arrays, functions, and objects.
+
+var myMusic = [
+ {
+   "artist": "Billy Joel",
+   "title": "Piano Man",
+   "release_year": 1973,
+   "formats": [
+     "CD",
+     "8T",
+     "LP"
+   ],
+   "gold": true
+ },
+ // Add record here
+ {
+   "artist": "Brandi Carlile",
+   "title": "Bear Creek",
+   "release_year": 2012,
+   "formats": [
+     "CD",
+     "8T",
+     "LP"
+   ],
+ }
+];
