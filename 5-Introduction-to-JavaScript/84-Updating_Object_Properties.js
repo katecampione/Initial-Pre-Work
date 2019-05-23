f489b7d65d545cfa973eb7339ec0e6be7f1ded71
Kate Campione <katecampione@gmail.com>
2019-05-23 12:15:09 -0600

    Updated the myDog objects name property

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,22 @@
+//I learned that after you've created a JavaScript object, you can update its properties at any time just like you would update any other variable. You can use either dot or bracket notation to update
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder"; // Updated the myDog objects name property
