939b947335e935a441b33f19ee65affdadb677b6
Kate Campione <katecampione@gmail.com>
2019-05-22 20:45:00 -0600

    Returned boolean functions

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,11 @@
+// I learned we can use the equality operator instead of if/else statements
+
+function isLess(a, b) {
+  // Fix this code
+  return a <= b;
+}
+
+
+
+// Change these values to test
+isLess(10, 15);
