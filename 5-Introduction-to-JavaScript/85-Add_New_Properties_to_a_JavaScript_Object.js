f48bc845ba49ddf5ac4217b1fb935947c7639224
Kate Campione <katecampione@gmail.com>
2019-05-23 12:17:20 -0600

    Added the bark property to an object

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// I learned that you can add new properties to an object just as you would edit existing properties.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "woof"; //added a bark property and set the sound to woof
