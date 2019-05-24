43d0b8f887187d10dc8fbb9289bbdf816fc8f0f2
Kate Campione <katecampione@gmail.com>
2019-05-24 10:56:54 -0600

    Wrote concise declarative functions

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,13 @@
+// I learned that with ES6, Yyu can remove the function keyword and colon altogether when defining functions in objects.
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
