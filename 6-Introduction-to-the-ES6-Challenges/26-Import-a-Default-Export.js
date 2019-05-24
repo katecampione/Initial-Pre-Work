f3018a98c9d2be5b422009aeec417d80409b8a9e
Kate Campione <katecampione@gmail.com>
2019-05-24 11:30:44 -0600

    Imported a default export

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,5 @@
+// I learned that to import a default export, you need to use a different import syntax. Unlike exported values, the primary method of importing a default export is to simply write the value's name after import.
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
