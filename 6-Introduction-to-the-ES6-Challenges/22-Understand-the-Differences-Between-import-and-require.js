81a0ec84998a4925dff502fbf6d308ae6435e064
Kate Campione <katecampione@gmail.com>
2019-05-24 11:15:22 -0600

    Differentiated between import and require

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+// I learned using 'import' we can choose which parts of a module or file to load into a given file, saving time and memory.
+
+"use strict";
+import { capitalizeString } from "string_functions";
+capitalizeString("hello!");
