d4dca64926665b5979bcb0c8f5b1aee8cadeffa9
Kate Campione <katecampione@gmail.com>
2019-05-23 22:45:52 -0600

    Wrote higher order arrow functions

---
+++ 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
@@ -0,0 +1,11 @@
+// I learned arrow functions work really well with higher order functions, such as map(), filter(), and reduce(), that take other functions as arguments for processing collections of data.
+
+const squareList = (arr) => {
+      "use strict";
+      const squaredIntegers = arr.filter( (num) => num > 0 && num % parseInt(num) === 0 ).map( (num) => Math.pow(num, 2) );
+      return squaredIntegers;
+    };
+
+    // test your code
+    const squaredIntegers = squareList(realNumberArray);
+    console.log(squaredIntegers);
